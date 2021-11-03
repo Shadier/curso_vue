fc621cdc6a64dfa2e3527ed0008e129cc003dcd7
Raúl Moreno <v-raul.moreno@aceable.com>
2021-11-02 21:02:59 -0600

    making posts now works

--- src/modules/timeline/Timeline.vue
+++ src/modules/timeline/Timeline.vue
@@ -1,9 +1,38 @@
 <template>
-    <div>
-        <h1>Timeline</h1>
+    <div class="timeline">
+        <Editor @post="createPost"/>
+        <template v-for="post in posts">
+            <Card :key="post.uid" :uid="post.uid" :content="post.content" :time-ago="post.date" />
+        </template>
     </div>
 </template>
 <script>
+import { v4 as uuid } from 'uuid';
+import Editor from './components/Editor';
+import Card from './components/Card';
+
+export default {
+    name: 'Timeline',
+    components: {
+        Editor,
+        Card,
+    },
+    data() {
+        return {
+            posts: [],
+        }
+    },
+    methods: {
+        createPost(content) {
+            const postBody = {
+                content,
+                uid: uuid(),
+                date: new Date(),
+            };
+            this.posts.push(postBody);
+        },
+    }
+}
 </script>
 <style lang="scss" scope>
 </style>
---
+++ src/modules/timeline/components/Editor.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <textarea v-model="message" :placeholder="placeholder"></textarea>
+        <button @click="dispatchPost">Publicar</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Editor',
+    props: {
+        placeholder: { type: String, default: 'Lorem ipsum...' },
+    },
+    data() {
+        return {
+            message: '',
+        }
+    },
+    methods: {
+        async dispatchPost() {
+            this.$emit('post', this.message);
+            this.message = '';
+        }
+    }
+}
+</script>

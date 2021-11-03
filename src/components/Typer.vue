ea3f6e69f859a7fd65e1184fb5ddd3737dcc688e
Raúl Moreno <v-raul.moreno@aceable.com>
2021-11-02 18:00:32 -0600

    first commit

---
+++ src/components/Typer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="typer">
+      <h1>Type your <em>Hello</em> message: </h1>
+      <input v-model="message" @input="onInput"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Typer',
+    data() {
+        return {
+            message: 'Welcome to your VueJS App',
+        }
+    },
+    methods: {
+        onInput() {
+            this.$emit('input-event', this.message)
+        },
+    }
+}
+</script>
+<style lang="scss" scoped>
+.typer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h1 {
+        font-size: 1em;
+        color: red;
+    }
+    input {
+        display: block;
+        width: 300px;
+        padding: 1em;
+        font-size: 1.4em;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="hellowMessage"/>
+    <Typer @input-event="onInput"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Typer from '@/components/Typer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Typer,
+  },
+  data() {
+    return {
+      hellowMessage: 'Welcome to your VueJS App',
+    }
+  },
+  created() {
+    console.log('El componente fué creado');
+  },
+  mounted() {
+    console.log('El componente está montado en nuestra UI');
+  },
+  destroyed() {
+    console.log('El componente home se destruyo');
+  },
+  methods: {
+    onInput(message) {
+      this.hellowMessage = message;
+    },
   }
 }
 </script>

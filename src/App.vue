15da205340c36050174aa6a281147435c0268899
Raúl Moreno <v-raul.moreno@aceable.com>
2021-11-02 19:15:24 -0600

    wrapper component created

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,57 @@
 <template>
   <div id="app" class="app">
-    <!-- TODO: App -->
+    <ThemeController :is-dark="isDark">
+      <div class="hello">
+        <h1>Hola mundo</h1>
+        <button @click="toggleTheme">Alternar tema</button>
+      </div>
+    </ThemeController>
   </div>
 </template>
+<script>
+import ThemeController from './shared/layouts/ThemeController.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ThemeController,
+  },
+  data() {
+    return {
+      isDark: false,
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.isDark = !this.isDark;
+    },
+  }
+}
+</script>
 
 <style lang="scss">
+.app {
+  .hello {
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    h1 {
+      font-size: 2em;
+      margin: .5em;
+    }
+    button {
+      display: block;
+      margin: .5em;
+      font-size: 1em;
+      padding: .6em 1em;
+      outline: 0;
+      border: 0;
+      border-radius: 2em;
+      cursor: pointer;
+    }
+  }
+}
 
 </style>
